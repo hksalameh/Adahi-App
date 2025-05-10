ae1dc83e6ef62a197668cc49b3381ab465f431c7
hksalameh <hksalameh@gmail.com>
2025-05-10 09:49:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAOV5iF6t5kdAeBIZPNcmINB4Ya66Yx9Pg", // استبدل بمفتاحك الفعلي
+    authDomain: "adahi-app.firebaseapp.com",
+    projectId: "adahi-app",
+    storageBucket: "adahi-app.firebasestorage.app",
+    messagingSenderId: "42370235858",
+    appId: "1:42370235858:web:6f839b92404afafdae9c0b",
+    measurementId: "G-1T2TGPK72K"
+};
 
+export const ADMIN_UID = 'AKpJLStGGnRC2Albb8bpZ0KtTGq1'; // استبدل بالـ UID الفعلي للمسؤول
